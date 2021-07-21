f95ccefe493bae98ff0a1ec2f2cd1b8fb8e2e9e1
ㄱ상찬 <sangchane@naver.com>
2021-07-21 10:14:50 +0000

    day 3 middlewares 3.5 to 3.10

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -2,25 +2,51 @@ import express from "express";
 
 const PORT = 4000;
 
+const date = new Date();
+const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
 const app = express(); // make application 
 
-const handleHome = (req, res) =>  {
-	return res.send("<h1>I still love you.</h1>");
-}
-const handleLogin = (req, res) => {
-	return res.send("<h2> Login here.</h2>");
-}
 
-const handleAbout = (req, res) => {
-	return res.send("<div>what we talk about??</div>");
+const urlLogger = (req, res, next) => {
+	console.log(`Path: ${req.url}`);	// localhost:4000/
+	next();
+};
+const timeLogger = (req, res, next) => {
+	console.log(`Time: ${year}.${month}.${day}`);	// ex 2021.07.21
+	next();
+};
+const securityLogger = (req, res, next) => {
+	const protocol = req.protocol;
+	if(protocol === "http"){
+		console.log(`Insecure: ❌`);	// http: Insecure X, https: Insecure O
+	}else{
+		console.log(`Insecure: ⭕`);
+	}
+	
+	next();
+};
+
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if(url === "/protected"){
+		return res.send("<h1>Not Allowed</h1>")
+	}
+	console.log("Allowed, you may continue.");
+	next();
 }
-const handleContact = (req, res) => {
-	return res.send("<ul>Contact Me 010 - 1234 - 5678 </ul>");
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handleProtected = (req, res) => {
+	return res.send("Welcome to the private lounge");
 }
-app.get("/", handleHome);		// request /
-app.get("/login", handleLogin);	// request /login
-app.get("/about", handleAbout);	// request /about
-app.get("/contact", handleContact);	// request /contact
+app.use(urlLogger);
+app.use(timeLogger);
+app.use(securityLogger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listenting on port http:/localhost:${PORT}`);
 
